7750679e5c73175f0f912a771a02aac195d984d6
faheem-445 <faheem44500@gmail.com>
2025-09-01 20:32:43 +0530

    Python Scripts

--- Python files/asd_adaboost.py
+++ Python files/asd_adaboost.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-
+fgzdv
 # Load the dataset
 data = pd.read_csv("Toddler Autism dataset.csv")
 
